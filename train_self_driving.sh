c3a74feabc576cea7fefe3aa658491dfb13f9016
ajhamdi <ajhamdi@yahoo.com>
2020-02-08 01:20:08 +0300

    final changes

--- train_self_driving.sh
+++
@@ -1,1 +0,0 @@
-python main.py --is_selfdrive=True --is_train=True --valid_size=50 --log_frq=10 --batch_size=64 --induced_size=50 --nb_paramters=3 --nb_steps=600  --learning_rate_t=0.0001 --learning_rate_g=0.0001
